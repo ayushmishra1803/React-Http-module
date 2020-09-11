b6b0825aca74b5e758e2bf6c0b2052ea063a66f1
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-09-12 02:10:17 +0530

    Updates

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+    state={
+        Post:[]
     }
+	componentDidMount() {
+		axios
+			.get("https://jsonplaceholder.typicode.com/posts")
+			.then((re) => {
+                this.setState({
+                    Post:re.data
+                })
+				console.log(this.state.Post);
+			})
+			.catch((re) => {
+				console.log(re);
+			});
+	}
+	render() {
+        const posts =this.state.Post.map((re,index)=>{
+          return <Post title={re.title} key={index} />;
+        })
+		return (
+			<div>
+				<section className="Posts">
+					{posts}
+				</section>
+				<section>
+					<FullPost />
+				</section>
+				<section>
+					<NewPost />
+				</section>
+			</div>
+		);
+	}
 }
 
 export default Blog;
