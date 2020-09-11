df0a6cb96defbd12bdd0ed4c81a8889fdbc53745
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-09-12 03:43:51 +0530

    Updates

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
 
 import "./FullPost.css";
+import axios from "axios";
 
 class FullPost extends Component {
+	componentDidUpdate() {
+		axios
+			.get("https://jsonplaceholder.typicode.com/posts/")
+			.then((re) => {
+				console.log(re);
+			})
+			.catch((re) => {
+				console.log(re);
+			});
+	}
 	render() {
 		let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
 		if (this.props.id) {
